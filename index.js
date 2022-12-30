baff4c98744f59dc27371fe6664e8a288c555574
Aura <ax.pink@gmail.com>
2022-12-30 13:03:54 -0500

    commit tree 797071a094993306cc20812d5b709e036904343a

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function customerNameUpperCase(customerName) {
+    return customerName.toUpperCase()
+}
+customerNameUpperCase('bob')
+
